af45a481f4f0d8038b30b3bdbd73f26479870c12
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-13 19:54:06 -0400

    axios url api updated

--- src/lib/loadCars.js
+++ src/lib/loadCars.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default async function loadCars() {
-  const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
+  // const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
+  const { data } = await axios.get("ford-mustang-lover-shop-next-8jgjq81r6-roddy-21-25.vercel.app/api/cars", { next: { revalidate: 3600 } });
   return data;
 }
 
