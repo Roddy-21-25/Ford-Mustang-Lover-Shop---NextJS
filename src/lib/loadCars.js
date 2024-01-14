3e3dbdf27b6c2847232a6ad3143abf371ad32793
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-14 00:22:33 -0400

    loadCars function updated

--- src/lib/loadCars.js
+++ src/lib/loadCars.js
@@ -1,8 +1,27 @@
 import axios from "axios";
 
+// export default async function loadCars() {
+//   const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
+//   const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/cars`, {
+//     next: { revalidate: 3600 },
+//   });
+//   return data;
+// }
+
 export default async function loadCars() {
-  // const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/cars`, { next: { revalidate: 3600 } });
-  return data;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/cars`);
+  const data = await response.json();
+
+  return { data: data };
 }
 
+// export async function getServerSideProps() {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/users`);
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       users: data,
+//     },
+//   };
+// }
