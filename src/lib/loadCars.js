1b683ef05a70191657bac415c89465a05064d1e2
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-13 23:27:06 -0400

    new env for Hos

--- src/lib/loadCars.js
+++ src/lib/loadCars.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function loadCars() {
   // const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
-  const { data } = await axios.get("https://ford-mustang-lover-shop-next-js.vercel.app/api/cars", { next: { revalidate: 3600 } });
+  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/cars`, { next: { revalidate: 3600 } });
   return data;
 }
 
