3fc818a93f53981817e9f56838e166c8d6254ca6
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-14 01:05:23 -0400

    railway

--- src/lib/loadCars.js
+++ src/lib/loadCars.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-// export default async function loadCars() {
-//   const { data } = await axios.get("http://localhost:3000/api/cars", { next: { revalidate: 3600 } });
-//   const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/cars`, {
-//     next: { revalidate: 3600 },
-//   });
-//   return data;
-// }
-
 export default async function loadCars() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/cars`);
-  const data = await response.json();
-
+  const { data } = await axios.get("https://fordmustang-api.up.railway.app/api/getCars", { next: { revalidate: 3600 } });
+  // const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/cars`, {
+  //   next: { revalidate: 3600 },
+  // });
   return data;
 }
+
+// export default async function loadCars() {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/cars`);
+//   const data = await response.json();
+
+//   return data;
+// }
 
 // export async function getServerSideProps() {
 //   const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/users`);
