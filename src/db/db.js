b0751666f78a094d82bb2d532dd89b616723a734
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-13 22:55:21 -0400

    prisma as a ORM with Mysql and PlanetScale

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,20 @@
-import mysql from "serverless-mysql";
+import { PrismaClient } from "@prisma/client";
+// import mysql from "serverless-mysql";
 
-export const conn = mysql({
-  config: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    // port: 3306,
-    database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-});
+// export const conn = mysql({
+//   config: {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     // port: 3306,
+//     database: process.env.DB_NAME,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+
+const prisma = new PrismaClient();
+
+export { prisma };
